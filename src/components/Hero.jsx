8454b57e237e062d1c79565c8a07c4ad1cf45814
TinySmash <islam.gueniari@gmail.com>
2023-05-03 22:00:27 +0100

    first section Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,17 +4,23 @@ import shoes from './images/shoes.png';
 export default function Hero() {
   return (
     <React.Fragment>
-      <section className="w-full h-screen relative bg-homepage-section-1 px-20">
-        <div className="absolute top-48">
-          <h1 className="text-5xl font-bold mb-2">Explore The New Kyrie 3</h1>
-          <h1 className="text-[4.25rem] font-bold text-primary-blue mb-2">
+      <section className="w-full h-screen relative bg-homepage-section-1 px-10 lg:px-20">
+        <div className="absolute w-4/5 sm:w-auto top-48 center-h sm:left-auto sm:translate-x-0">
+          <h1 className="text-3xl md:text-5xl font-bold mb-3 md:mb-6 ">
+            Explore The New Kyrie 3
+          </h1>
+          <h1 className="text-4xl lg:text-[4.25rem] font-bold text-primary-blue mb-4 md:mb-10">
             Bruce LEE
           </h1>
           <button className="w-52 h-auto text-3xl text-white bg-primary-blue font-bold px-4 py-3 rounded-full">
             BUY NOW
           </button>
         </div>
-        <img src={shoes} className="absolute w-[45%] right-16 top-28" alt="" />
+        <img
+          src={shoes}
+          className="center-h w-[80%] sm:w-[45%] right-16 top-[60%] sm:top-48 lg:top-28 sm:left-auto sm:translate-x-0"
+          alt=""
+        />
       </section>
     </React.Fragment>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import shoppingCart from './images/shopping-cart.png';
-import person from './images/person.png';
+import { Person, ShoppingCartRounded } from '@mui/icons-material';
 
 export default function Navbar() {
   return (
-    <header className="w-11/12 mx-[4.33333%] fixed flex justify-between items-center px-12 py-[0.4rem] h-16 md:h-20 border-b-2 border-black z-50">
-      <h1 className="text-4xl font-bold">Hoopin'</h1>
-      <nav className="w-3/4 sm:w-2/3 lg:w-1/2 h-4/5 flex items-center justify-evenly md:justify-between">
+    <header className="w-11/12 mx-[4.33333%] fixed flex justify-between items-center px-4 sm:px-12 py-[0.4rem] h-16 md:h-20 border-b-2 border-black z-50">
+      <h1 className="text-3xl md:text-4xl font-bold">Hoopin'</h1>
+      <nav className="w-3/4 sm:w-2/3 lg:w-1/2 h-4/5 flex items-center justify-start md:justify-between">
         <ul className="list-none hidden w-3/5 md:flex justify-between text-xl font-semibold mr-[5%]">
           <li>
             <Link>Categories</Link>
@@ -19,13 +18,13 @@ export default function Navbar() {
             <Link>Support</Link>
           </li>
         </ul>
-        <button className="w-10 md:hidden md:w-16 h-full border-2 border-black rounded-md"></button>
-        <div className="w-[30%] md:w-2/5 h-full flex justify-end relative">
-          <div className="w-[3.2rem] h-full rounded-full bg-primary-gray mr-[20%] border-2 cursor-pointer p-2">
-            <img src={shoppingCart} alt="" />
+        <button className=" w-8 sm:w-10 ml-[20%] sm:ml-[30%] md:hidden h-8 sm:h-full border-2 border-black rounded-md"></button>
+        <div className="w-1/3 md:w-2/5 h-full absolute right-4 sm:right-12 flex justify-end py-3">
+          <div className="flex items-center justify-center w-10 md:w-[3.3rem] h-full rounded-full bg-primary-gray cursor-pointer mr-4 sm:mr-8">
+            <ShoppingCartRounded></ShoppingCartRounded>
           </div>
-          <div className="w-[3.2rem] h-full rounded-full bg-primary-gray border-2 cursor-pointer p-2">
-            <img src={person} alt="" />
+          <div className="flex items-center justify-center  w-10 md:w-[3.3rem] h-full rounded-full bg-primary-gray cursor-pointer">
+            <Person sx={{ fontSize: '30px' }}></Person>
           </div>
         </div>
       </nav>
