10288f8823966fc823a9353114ebb72ad0709765
TinySmash <islam.gueniari@gmail.com>
2023-05-17 19:07:12 +0100

    second section done

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -22,7 +22,7 @@ export default function Hero() {
   return (
     <React.Fragment>
       <section className="w-full h-screen relative bg-homepage-section-1 px-10 lg:px-20">
-        <div className=" absolute center-h w-4/5 sm:w-auto top-28 sm:top-48 mx-auto sm:left-auto sm:translate-x-0">
+        <div className="absolute center-h md:uncenter-h w-4/5 sm:w-auto top-28 sm:top-48 mx-auto">
           <h1 className="text-4xl md:text-5xl font-bold mb-3 md:mb-6 ">
             Explore The New Kyrie 3
           </h1>
@@ -35,41 +35,42 @@ export default function Hero() {
         </div>
         <img
           src={shoes}
-          className="absolute center-h w-[90%] sm:w-[45%] right-16 top-[50%] sm:top-48 lg:top-28 sm:left-auto sm:translate-x-0"
+          className="absolute center-h w-[90%] sm:w-[45%] sm:right-16 top-[50%] sm:top-48 lg:top-28 sm:uncenter-h mb-8"
           alt=""
         />
       </section>
-      <section className="w-full min-h-screen relative px-10 lg:px-20 bg-primary-gray bg-homepage-section-2">
+      <section className="w-full min-h-screen md:h-screen relative px-10 pb-5 lg:px-20 bg-primary-gray bg-homepage-section-2 md:flex md:justify-between md:items-center">
         <img
           src={sec2}
-          className="w-full sm:w-4/6 md:w-1/2 relative center-h top-16 mb-10"
+          className="w-full mx-auto sm:w-4/6 md:w-1/2 relative md:absolute center-h md:uncenter-h md:top-auto md:right-0 mb-0 md:mb-0 "
           alt=""
         />
-        <ul className="list-none w-full px-2 py-3">
-          <li className=" w-[90%] h-56 border-2 border-black mb-6 mx-auto rounded-3xl text-center px-2 py-5 bg-nav-primary-gray backdrop-blur-md">
+        <ul className="list-none w-full relative md:w-1/2 lg:w-2/5">
+          {/* md:top-[84pex] */}
+          <li className=" w-[90%]  h-56 md:h-fit border-2 border-black mb-6 md:mb-3 mx-auto rounded-3xl text-center px-2 py-5 md:py-3 bg-nav-primary-gray backdrop-blur-md">
             <h1 className="text-dark-yellow text-3xl font-bold mb-4">
               Comfort
             </h1>
-            <p>
+            <p className="text-sm sm:text-lg ">
               Stylish and comfortable shoe designed for basketball players. Its
               cushioned sole and innovative technology offer superior support ,
               and a soft experience on the court
             </p>
           </li>
-          <li className="w-[90%] h-56 border-2 border-black mb-6 mx-auto rounded-3xl text-center px-2 py-5 bg-nav-primary-gray backdrop-blur-md">
+          <li className="w-[90%] h-56 md:h-fit border-2 border-black mb-6 md:mb-3 mx-auto rounded-3xl text-center px-2 py-5 md:py-3 bg-nav-primary-gray backdrop-blur-md">
             <h1 className="text-dark-yellow text-3xl font-bold mb-4">Hybrid</h1>
-            <p>
+            <p className="text-sm sm:text-lg ">
               {' '}
               Blends elements of Kyrie Irving's signature shoe with the iconic
               design of Kobe Bryant's shoes. This fusion results in a unique
               shoe that offers both comfort and style for basketball players.
             </p>
           </li>
-          <li className=" w-[90%] h-56 border-2 border-black mb-6 mx-auto rounded-3xl text-center px-2 py-5 bg-nav-primary-gray backdrop-blur-md">
+          <li className=" w-[90%] h-56 md:h-fit border-2 border-black mx-auto rounded-3xl text-center px-2 py-5 md:py-3 bg-nav-primary-gray backdrop-blur-md">
             <h1 className="text-dark-yellow text-3xl font-bold mb-4">
               Challenge
             </h1>
-            <p>
+            <p className="text-sm sm:text-lg ">
               The Nike Kyrie 3 Bruce Lee edition challenges players to raise
               their game with its unique design and cutting-edge technology,
               inspiring them to push their limits on the court.
@@ -77,6 +78,7 @@ export default function Hero() {
           </li>
         </ul>
       </section>
+      <section className="w-full h-screen bg-primary-gray"></section>
     </React.Fragment>
   );
 }
