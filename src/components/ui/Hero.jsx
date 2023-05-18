0ff8b93632bb8796451d74f94723ca81d001aec8
TinySmash <islam.gueniari@gmail.com>
2023-05-18 20:17:02 +0100

    all sections done, footer left

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import shoes from '../images/shoes.png';
 import sec2 from '../images/sec2.png';
+import sec3 from '../images/sec3.png';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { Login, Logout, SignUp } from '../reducers/userSlice';
 
@@ -21,7 +22,7 @@ export default function Hero() {
 
   return (
     <React.Fragment>
-      <section className="w-full h-screen relative bg-homepage-section-1 px-10 lg:px-20">
+      <section className="w-full h-screen relative bg-homepage-section-1 px-7 sm:px-10 lg:px-20">
         <div className="absolute center-h md:uncenter-h w-4/5 sm:w-auto top-28 sm:top-48 mx-auto">
           <h1 className="text-4xl md:text-5xl font-bold mb-3 md:mb-6 ">
             Explore The New Kyrie 3
@@ -39,46 +40,89 @@ export default function Hero() {
           alt=""
         />
       </section>
-      <section className="w-full min-h-screen md:h-screen relative px-10 pb-5 lg:px-20 bg-primary-gray bg-homepage-section-2 md:flex md:justify-between md:items-center">
-        <img
-          src={sec2}
-          className="w-full mx-auto sm:w-4/6 md:w-1/2 relative md:absolute center-h md:uncenter-h md:top-auto md:right-0 mb-0 md:mb-0 "
-          alt=""
-        />
-        <ul className="list-none w-full relative md:w-1/2 lg:w-2/5">
-          {/* md:top-[84pex] */}
-          <li className=" w-[90%]  h-56 md:h-fit border-2 border-black mb-6 md:mb-3 mx-auto rounded-3xl text-center px-2 py-5 md:py-3 bg-nav-primary-gray backdrop-blur-md">
-            <h1 className="text-dark-yellow text-3xl font-bold mb-4">
-              Comfort
+      <section className="bg-homepage-section-2 bg-primary-gray">
+        <section className="w-full min-h-screen md:h-screen relative px-10 pb-5 lg:px-20  md:flex md:justify-between md:items-center">
+          <img
+            src={sec2}
+            className="w-full mx-auto sm:w-4/6 md:w-1/2 relative md:absolute center-h md:uncenter-h md:top-auto md:right-0 mb-0 md:mb-0 "
+            alt=""
+          />
+          <ul className="list-none w-full relative md:w-1/2 lg:w-2/5">
+            {/* md:top-[84pex] */}
+            <li className=" w-[90%]  h-56 md:h-fit border-2 border-black mb-6 md:mb-3 mx-auto rounded-3xl text-center px-2 py-5 md:py-3 bg-nav-primary-gray backdrop-blur-md">
+              <h1 className="text-dark-yellow text-3xl font-bold mb-4">
+                Comfort
+              </h1>
+              <p className="text-sm sm:text-lg ">
+                Stylish and comfortable shoe designed for basketball players.
+                Its cushioned sole and innovative technology offer superior
+                support , and a soft experience on the court
+              </p>
+            </li>
+            <li className="w-[90%] h-56 md:h-fit border-2 border-black mb-6 md:mb-3 mx-auto rounded-3xl text-center px-2 py-5 md:py-3 bg-nav-primary-gray backdrop-blur-md">
+              <h1 className="text-dark-yellow text-3xl font-bold mb-4">
+                Hybrid
+              </h1>
+              <p className="text-sm sm:text-lg ">
+                {' '}
+                Blends elements of Kyrie Irving's signature shoe with the iconic
+                design of Kobe Bryant's shoes. This fusion results in a unique
+                shoe that offers both comfort and style for basketball.
+              </p>
+            </li>
+            <li className=" w-[90%] h-56 md:h-fit border-2 border-black mx-auto rounded-3xl text-center px-2 py-5 md:py-3 bg-nav-primary-gray backdrop-blur-md">
+              <h1 className="text-dark-yellow text-3xl font-bold mb-4">
+                Challenge
+              </h1>
+              <p className="text-sm sm:text-lg ">
+                The Nike Kyrie 3 Bruce Lee edition challenges players to raise
+                their game with its unique design and cutting-edge technology,
+                inspiring them to push their limits on the court.
+              </p>
+            </li>
+          </ul>
+        </section>
+        <section className="w-full h-auto md:h-screen relative px-7 sm:px-10 lg:px-20 py-5 ">
+          <div className="md:absolute w-full md:w-1/2 right-10 lg:right-20 my-5 md:my-0 md:top-20 p-2 sm:p-5 md:bg-nav-primary-gray md:backdrop-blur-md rounded-lg text-center md:text-start">
+            <h1 className="text-3xl sm:text-5xl text-dark-yellow font-semibold mb-5">
+              Nike Exclusive for the first time
             </h1>
-            <p className="text-sm sm:text-lg ">
-              Stylish and comfortable shoe designed for basketball players. Its
-              cushioned sole and innovative technology offer superior support ,
-              and a soft experience on the court
+            <h2 className="text-2xl sm:text-3xl font-semibold leading-relaxed mb-8">
+              Kyrie 3 Mamba mentality. <br />
+              <span className="underline underline-offset-4">120$</span>
+            </h2>
+            <p className="hidden md:flex">
+              The famous quote by Bruce Lee, "Be yourself. Express yourself.
+              Have faith in yourself," strongly influenced Kyrie Irving and Kobe
+              Bryant. It inspired the Mamba Mentality and is reflected in the
+              KYRIE 3 Mamba Mentality shoes, paying tribute to Bruce Lee and
+              Kobe. The colorway and design symbolize their connection,
+              featuring elements like Bruce Lee's colors and a samurai-inspired
+              pattern. The shoe's tongue bears the Kyrobe logo, combining Bryant
+              and Irving's logos.
             </p>
-          </li>
-          <li className="w-[90%] h-56 md:h-fit border-2 border-black mb-6 md:mb-3 mx-auto rounded-3xl text-center px-2 py-5 md:py-3 bg-nav-primary-gray backdrop-blur-md">
-            <h1 className="text-dark-yellow text-3xl font-bold mb-4">Hybrid</h1>
-            <p className="text-sm sm:text-lg ">
-              {' '}
-              Blends elements of Kyrie Irving's signature shoe with the iconic
-              design of Kobe Bryant's shoes. This fusion results in a unique
-              shoe that offers both comfort and style for basketball players.
-            </p>
-          </li>
-          <li className=" w-[90%] h-56 md:h-fit border-2 border-black mx-auto rounded-3xl text-center px-2 py-5 md:py-3 bg-nav-primary-gray backdrop-blur-md">
-            <h1 className="text-dark-yellow text-3xl font-bold mb-4">
-              Challenge
-            </h1>
-            <p className="text-sm sm:text-lg ">
-              The Nike Kyrie 3 Bruce Lee edition challenges players to raise
-              their game with its unique design and cutting-edge technology,
-              inspiring them to push their limits on the court.
-            </p>
-          </li>
-        </ul>
+          </div>
+          <img
+            src={sec3}
+            className="w-2/3 md:w-1/3 relative mx-auto md:mx-0 md:top-20 rounded-md"
+            alt=""
+          />
+        </section>
       </section>
-      <section className="w-full h-screen bg-primary-gray"></section>
+      <section className="w-full h-screen px-7 sm:px-10 lg:px-20 py-5 bg-primary-gray bg-homepage-section-4 relative">
+        <div className="w-full text-center center-both px-2 sm:px-10 lg:px-20">
+          <h1 className="text-2xl sm:text-3xl md:text-5xl font-semibold leading-snug md:leading-[1.5] mb-7">
+            Discover more basketball shoes & other at{' '}
+            <span className="text-primary-blue text-3xl sm:text-4xl md:text-6xl">
+              Hoopin'
+            </span>{' '}
+            <br /> <span className="text-dark-yellow">+ Special offers</span>
+          </h1>
+          <button className="text-3xl text-white bg-primary-blue font-bold px-4 py-3 rounded-full">
+            Explore Now
+          </button>
+        </div>
+      </section>
     </React.Fragment>
   );
 }
