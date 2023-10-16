0cc6fbc4805d0b294137203dc43a1e8e593463db
TinySmash <islam.gueniari@gmail.com>
2023-10-16 18:42:48 +0100

    navbar spacing design

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -92,7 +92,7 @@ export default function Navbar() {
         </ul>
       </div>
       <header
-        className="w-11/12 mx-[4.33333%] fixed flex justify-between items-center px-4 sm:px-12 py-[0.4rem] h-16 md:h-20 bg-transparent border-b-2 border-black z-40 transition-all duration-300"
+        className="w-11/12 mx-[4.33333%] fixed flex justify-between items-center px-4 sm:px-10 md:px-12 py-[0.4rem] h-16 md:h-20 bg-transparent border-b-2 border-black z-40 transition-all duration-300"
         ref={navbarRef}
       >
         <h1
@@ -101,55 +101,49 @@ export default function Navbar() {
         >
           Hoopin'
         </h1>
-        <nav className="w-3/4 sm:w-2/3 lg:w-1/2 h-4/5 flex items-center justify-start md:justify-between">
-          <ul className="list-none hidden w-3/5 md:flex justify-between text-xl font-semibold mr-[5%]">
-            <li>
-              <Link className="cursor-pointer" to="/shop">
-                Shop
-              </Link>
-            </li>
-            <li>
-              <Link className="cursor-pointer" to="about">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link className="cursor-pointer">Support</Link>
-            </li>
-          </ul>
-          <button
-            className=" w-8 sm:w-10 ml-[20%] sm:ml-[30%] md:hidden h-8 sm:h-full border-2 border-black rounded-md"
-            onClick={toggleMenu}
-          >
-            <Menu sx={{ fontSize: "25px" }}></Menu>
-          </button>
-          <div className="w-1/3 md:w-2/5 h-full absolute right-4 sm:right-12 flex justify-end py-3 pl-5 sm:pl-auto">
-            <button
-              className="flex items-center justify-center w-10 md:w-[3.3rem] h-full rounded-full bg-primary-gray cursor-pointer mr-3 sm:mr-8"
-              onClick={() => {
-                !userLoginInfo.isConnected
-                  ? navigate("/signup")
-                  : navigate(
-                      `/user/${userLoginInfo.userId}/${userLoginInfo.username}/saved-products`
-                    );
-              }}
-            >
-              <ShoppingCartRounded></ShoppingCartRounded>
-            </button>
-            <button
-              className="flex items-center justify-center  w-10 md:w-[3.3rem] h-full rounded-full bg-primary-gray cursor-pointer"
-              onClick={() => {
-                !userLoginInfo.isConnected
-                  ? navigate("/signup")
-                  : navigate(
-                      `/user/${userLoginInfo.userId}/${userLoginInfo.username}`
-                    );
-              }}
-            >
-              <Person sx={{ fontSize: "30px" }}></Person>
-            </button>
-          </div>
+        <nav className="w-fit h-4/5 hidden md:flex gap-12 items-center text-xl font-semibold">
+          <Link className="cursor-pointer" to="/shop">
+            Shop
+          </Link>
+
+          <Link className="cursor-pointer" to="/about">
+            About
+          </Link>
+
+          <Link className="cursor-pointer">Support</Link>
         </nav>
+        <button
+          className=" w-8 sm:w-10 md:hidden h-8 sm:h-10 border-2 border-black rounded-md"
+          onClick={toggleMenu}
+        >
+          <Menu sx={{ fontSize: "28px" }}></Menu>
+        </button>
+        <div className="w-fit h-full relative flex gap-2 sm:gap-5 justify-end py-3">
+          <button
+            className="flex items-center justify-center w-fit p-[10px] h-full rounded-full bg-primary-gray cursor-pointer"
+            onClick={() => {
+              !userLoginInfo.isConnected
+                ? navigate("/signup")
+                : navigate(
+                    `/user/${userLoginInfo.userId}/${userLoginInfo.username}/saved-products`
+                  );
+            }}
+          >
+            <ShoppingCartRounded></ShoppingCartRounded>
+          </button>
+          <button
+            className="flex items-center justify-center  w-fit p-[10px] h-full rounded-full bg-primary-gray cursor-pointer"
+            onClick={() => {
+              !userLoginInfo.isConnected
+                ? navigate("/signup")
+                : navigate(
+                    `/user/${userLoginInfo.userId}/${userLoginInfo.username}`
+                  );
+            }}
+          >
+            <Person sx={{ fontSize: "30px" }}></Person>
+          </button>
+        </div>
       </header>
     </>
   );
